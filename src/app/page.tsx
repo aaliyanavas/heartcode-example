5f68c454d6d89e91c6a3a254480d6e707e8239c6
Aaliya Navas <aaliyanavas05@gmail.com>
2024-10-09 09:28:27 +0800

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <div className="bg-[url('/naruto2.jpg')]">
       <div className="flex flex-col justify-center h-dvh ">
-        <p className="font-bold text-5xl text-center">Dont do drugs</p>
-        <p className="text-sm text-center">drugs are bad!!</p>
+        <p className="font-bold text-5xl text-center">Dont Do Drugs</p>
+        <p className="text-sm text-center">Drugs Are Bad!!!</p>
       </div>
     </div>
   );
