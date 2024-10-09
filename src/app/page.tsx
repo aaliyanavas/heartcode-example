7255afb6cd21e58ed9863e254f884562894d552e
Aaliya Navas <aaliyanavas05@gmail.com>
2024-10-09 10:19:48 +0800

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
-export default function Home() {
+export default function Home () {
   return (
-    <div className="bg-[url('/naruto2.jpg')]">
+    <div className="bg-[url('/sudoku.jpeg')]">
       <div className="flex flex-col justify-center h-dvh ">
         <p className="font-bold text-5xl text-center">Dont Do Drugs</p>
         <p className="text-sm text-center">Drugs Are Bad!!!</p>
       </div>
     </div>
   );
+}
+import { FlipWords } from "@/components/ui/flip-words";
+
+export default function Home () {
+  const words: string[] = ["skibidi", "litty", "sigma"];
+
+  return (
+	<div>
+  	<div className="flex flex-col h-[calc(100vh-60px)] items-center justify-center">
+    	<h1 className="font-bold text-5xl">Taking drugs is not
+      	<FlipWords words={words}/>
+    	</h1>
+  	</div>
+	</div>
+  )
 }
